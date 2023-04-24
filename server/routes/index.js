8f473a6dc3857b7870783823fee8d10e71bf24b0
Gundeep Singh Saluja <gssaluja77@gmail.com>
2023-04-23 23:52:44 -0400

    Configured the routes for community posts page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,15 @@
+import communityPostsRoute from "./communityPosts.js";
+import commentsRoute from "./comments.js";
+import likesRoute from "./likes.js";
+
 const constructorMethod = (app) => {
-    app.use("*", (req, res) => {
-        res.status(404).send("Page Not Found!");
-    })
-}
+  app.use("/community-posts", communityPostsRoute);
+  app.use("/view-post", commentsRoute);
+  app.use("/likes", likesRoute);
+  
+  app.use("*", (req, res) => {
+    res.status(404).send("Page Not Found!");
+  });
+};
 
 export default constructorMethod;
