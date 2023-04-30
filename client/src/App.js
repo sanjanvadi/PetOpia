57b77b87a0ef58a4959fd23870c2db3657daff3e
Gundeep Singh Saluja <gssaluja77@gmail.com>
2023-04-30 07:32:12 -0400

    Initialized React app for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import { React } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import CommunityPosts from "./components/CommunityPosts";
+import ViewPost from "./components/ViewPost";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <header>
+          <nav className="App-header">
+              <h1 className="App-title">PetOpia</h1>
+            <div className="navLinks">
+              <NavLink className="post-link nav-link" to="/">
+                Home
+              </NavLink>
+            </div>
+          </nav>
+        </header>
+        <Routes>
+          <Route path={`/community-posts`} element={<CommunityPosts />} />
+          <Route path={`/community-posts/:postId`} element={<ViewPost />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
