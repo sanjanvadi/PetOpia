928508bb4975035a1a9bf4d3dd92f9d51f421259
mferguso1 <mdfwc20@gmail.com>
2023-05-09 23:35:47 -0400

    Remove compiler warning

--- client/src/components/ViewPost.js
+++ client/src/components/ViewPost.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import {
   Card,
